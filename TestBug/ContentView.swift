7f4eb7dd3336a2c31273482cb181539462a5721b
eegrok <304507+eegrok@users.noreply.github.com>
2023-02-04 11:28:55 +1300

    try reproducing bug with bool sheet -- doesn't repro

--- TestBug/ContentView.swift
+++ TestBug/ContentView.swift
@@ -8,15 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var show: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+      VStack {
+        Image(systemName: "globe")
+          .imageScale(.large)
+          .foregroundColor(.accentColor)
+        Text("Hello, world!")
+        Button("Open Sheet") {
+          self.show = true
+        }
+        .padding(10).border(show ? Color.red : Color.clear)
+        .sheet(isPresented: $show,
+               onDismiss: { print("finished!") },
+               content: { MySheet() })
+      
         }
         .padding()
     }
+}
+
+struct MySheet: View {
+  	@State private var day: String = ""
+    @Environment (\.presentationMode) var presentationMode
+
+    var body: some View {
+        VStack(spacing: 20) {
+          Spacer()
+          Menu(content: menuContents, label: menuLabel)
+              .frame(width: 200)
+            Text("Drag down to dismiss..., or")
+          Spacer()
+            Text("Tap to Dismiss")
+                .onTapGesture {
+                    self.presentationMode.wrappedValue.dismiss()
+            }
+					Spacer()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.green)
+        .edgesIgnoringSafeArea(.all)
+    }
+  @ViewBuilder func menuContents() -> some View {
+      Button("All Days") { self.day = "All Days"}
+      
+      Menu("Working Day") {
+          Button("Monday") { self.day = "Monday" }
+          Button("Tuesday") { self.day = "Tuesday" }
+          Button("Wednesday") { self.day = "Wednesday" }
+          Button("Thursday") { self.day = "Thursday" }
+          Button("Friday") { self.day = "Friday" }
+      }
+      
+      // This view is required to avoid SwiftUI merging menus "Working Day" and "Weekend"
+      Color.clear.frame(width: 1, height: 1)
+      
+      Menu("Weekend") {
+          Button("Saturday") { self.day = "Saturday" }
+          Button("Sunday") { self.day = "Sunday" }
+      }
+  }
+  
+  @ViewBuilder func menuLabel() -> some View {
+      HStack {
+          Image(systemName: "calendar")
+          
+          Text("Select Day")
+      }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
