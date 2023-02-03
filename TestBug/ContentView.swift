6271a01003815f466e5ab5b2c810c78c421e7585
eegrok <304507+eegrok@users.noreply.github.com>
2023-02-04 10:47:23 +1300

    Initial Commit

---
+++ TestBug/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TestBug
+//
+//  Created by Kem Mason on 4/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestBug/TestBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBugApp.swift
+//  TestBug
+//
+//  Created by Kem Mason on 4/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TestBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
